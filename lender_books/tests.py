383860ec732dd2d9bf416c6b28b610e4b25d932b
LutherMckeiver <leoluther96@gmail.com>
2018-09-20 22:18:50 -0700

    added readme, docstrings, fixed functions, did some test

--- lender_books/tests.py
+++ lender_books/tests.py
@@ -1,49 +1,61 @@
 from django.test import TestCase, RequestFactory
 from .models import Book
-
+from django.contrib.auth.models import User
 
 
 class TestBookModel(TestCase):
     def setUp(self):
-        self.book = Book.objects.create(title='Hello', author='Goodbye')
-        Book.objects.create(title='Desk', author='Chair')
-        Book.objects.create(title='Wall', author='Whiteboard')
+        self.user = User.objects.create(username='test', email='testemail@gmail.com')
+        self.user.set_password('dudebroguyman')
+        self.book = Book.objects.create(
+            title='Django',
+            author='Python',
+            user=self.user
+        )
+        Book.objects.create(title='Late Nights At Code Fellows', author='401 Student', user=self.user)
+        Book.objects.create(title='Sapiens', author='Yuval Noah Harari', user=self.user)
 
     def test_book_titles(self):
         """
-
-        :return: This function asserts that titles are correct
+        :return: Test if title is correct
         """
-        self.assertEqual(self.book.title, 'Hello')
+        self.assertEqual(self.book.title, 'Django')
 
     def test_book_author(self):
-        """"""
-        self.assertEqual(self.book.author, 'Goodbye')
+        """
+        :return: Testing if author is correct
+        """
+        self.assertEqual(self.book.author, '401 Student')
 
-    def test_wrong_title(self):
-        book = Book.objects.get(author='Chair')
+    def test_book_detail(self):
+        """
+        :return: Test if book detail is correct
+        """
+        book = Book.objects.get(author='Yuval Noah Harari')
 
-        self.assertEqual(book.title, 'Desk')
-
-    def test_wrong_author(self):
-        book = Book.objects.get(author='Wall')
-
-        self.assertEqual(book.author, 'Whiteboard')
+        self.assertEqual(book.title, 'Sapiens')
 
 
 class TestBookViews(TestCase):
     def setUp(self):
-        self.request = RequestFactory()
-        self.book_one = Book.objects.create(title='Cool', author='Omg wow')
-        self.book_two = Book.objects.create(title='Beans', author='Jelly Beans')
+        self.user = User.objects.create(username='Peter Parker', email='imnotspiderman@spidy.com')
+        self.user.set_password('maryjane')
+        self.book_one = Book.objects.create(title='Lion', author='Tiger')
+        self.book_two = Book.objects.create(title='Tiger', author='Lion')
 
     def test_book_detail_view(self):
+        """
+        :return: Testing the book detail view
+        """
         from .views import book_detail_view
         request = self.request.get('')
         response = book_detail_view(request, f'{self.book_one.id}')
-        self.assertIn(b'Omg wow', response.content)
+        self.assertIn(b'Tiger', response.content)
 
     def test_book_detail_status(self):
+        """
+        :return: Testing if the status is correct
+        """
         from .views import book_detail_view
         request = self.request.get('')
         response = book_detail_view(request, f'{self.book_one.id}')
